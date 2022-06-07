9566a1d1ff73b426da5b0c4fd9d8b876788687ea
Daphne2002 <549230216@qq.com>
2022-06-08 01:23:17 +0800

    First commit.

---
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sunnyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//cd D:\\MyApplication\\SunnyWeather
+// git clone https://github.com/Daphne2002/SunnyWeather.git
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
